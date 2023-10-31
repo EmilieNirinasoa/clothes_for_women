9b600407d4215bb2a556c1f0ead13787e7a7d22e
EmilieNirinasoa <fanomezanaemilienirinasoa@gmail.com>
2023-10-31 15:49:12 +0300

    dependances

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:oktoast/oktoast.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+import 'core/constant/strings.dart';
+import 'core/router/app_router.dart';
+import 'core/theme/app_theme.dart';
+import 'domain/usecases/product/get_product_usecase.dart';
+import 'presentation/blocs/cart/cart_bloc.dart';
+import 'presentation/blocs/category/category_bloc.dart';
+import 'presentation/blocs/delivery_info/delivery_info_action/delivery_info_action_cubit.dart';
+import 'presentation/blocs/delivery_info/delivery_info_fetch/delivery_info_fetch_cubit.dart';
+import 'presentation/blocs/filter/filter_cubit.dart';
+
+import 'core/services/services_locator.dart' as di;
+import 'presentation/blocs/home/navbar_cubit.dart';
+import 'presentation/blocs/order/order_fetch/order_fetch_cubit.dart';
+import 'presentation/blocs/product/product_bloc.dart';
+import 'presentation/blocs/user/user_bloc.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+
   runApp(const MyApp());
+  configLoading();
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(199, 255, 3, 15)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(
-          "EShop",
-          style: TextStyle(color: Color.fromARGB(235, 253, 252, 252)),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => NavbarCubit(),
+        ),
+        BlocProvider(
+          create: (context) => FilterCubit(),
+        ),
+        BlocProvider(
+          create: (context) => di.sl<ProductBloc>()
+            ..add(const GetProducts(FilterProductParams())),
+        ),
+        BlocProvider(
+          create: (context) =>
+              di.sl<CategoryBloc>()..add(const GetCategories()),
+        ),
+        BlocProvider(
+          create: (context) => di.sl<CartBloc>()..add(const GetCart()),
+        ),
+        BlocProvider(
+          create: (context) => di.sl<UserBloc>()..add(CheckUser()),
+        ),
+        BlocProvider(
+          create: (context) => di.sl<DeliveryInfoActionCubit>(),
+        ),
+        BlocProvider(
+          create: (context) =>
+              di.sl<DeliveryInfoFetchCubit>()..fetchDeliveryInfo(),
+        ),
+        BlocProvider(
+          create: (context) => di.sl<OrderFetchCubit>()..getOrders(),
+        ),
+      ],
+      child: OKToast(
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: AppRouter.home,
+          onGenerateRoute: AppRouter.onGenerateRoute,
+          title: appTitle,
+          theme: AppTheme.lightTheme,
+          builder: EasyLoading.init(),
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.only(bottom: 8),
-            child: Text("Vetements",
-                style: TextStyle(fontWeight: FontWeight.bold)),
-          ),
-          Text(
-            "A vendre",
-            style: TextStyle(color: Colors.grey[500]),
-          ),
-        ],
-      ),
     );
   }
+}
+
+void configLoading() {
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 2500)
+    ..indicatorType = EasyLoadingIndicatorType.fadingCircle
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..progressColor = Colors.white
+    ..backgroundColor = Colors.black
+    ..indicatorColor = Colors.white
+    ..textColor = Colors.white
+    ..userInteractions = false
+    ..maskType = EasyLoadingMaskType.black
+    ..dismissOnTap = true;
 }
